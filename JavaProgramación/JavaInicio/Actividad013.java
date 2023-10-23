49817278e3777eafd8658d15f676487dc3071088
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-10-23 18:30:36 +0200

    mascosas

---
+++ JavaProgramación/JavaInicio/Actividad013.java
@@ -0,0 +1,19 @@
+package JavaInicio;
+
+import java.util.Scanner;
+
+public class Actividad013 {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriba un carácter: ");
+        char caracter = scanner.next().charAt(0);
+        System.out.println("Escriba otro carácter: ");
+        char caracter1 = scanner.next().charAt(0);
+        if (caracter == caracter1) {
+            System.out.println("Los dos carácteres son iguales");
+        } else {
+            System.out.println("Los dos carácteres no son los mismos");
+        }
+        scanner.close();
+    }
+}
