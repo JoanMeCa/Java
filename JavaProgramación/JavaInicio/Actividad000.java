f0673c6a484a3e405afdb6960b04e5539607de0a
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-10-25 15:10:01 +0200

    ups

---
+++ JavaProgramación/JavaInicio/Actividad000.java
@@ -0,0 +1,21 @@
+package JavaInicio;
+
+import java.util.Scanner;
+
+public class Actividad000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un string: ");
+        String inputString = scanner.nextLine();
+        char[] caracteres = inputString.toCharArray();
+        int conteo = 0;
+        for (char caracter : caracteres) {
+            if (Character.toUpperCase(caracter) == 'O') {
+                conteo++;
+            }
+        }
+        System.out.println("En el string introducido hay un total de " + conteo + " o's.");
+        scanner.close();
+    }
+}
