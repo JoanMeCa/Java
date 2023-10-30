38b37a863c48ada48fc4147703185f07f54a74ac
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-10-30 16:24:08 +0100

    cosas

---
+++ JavaProgramación/JavaInicio/Actividadpostexamen.java
@@ -0,0 +1,19 @@
+package JavaInicio;
+
+import java.util.Scanner;
+
+public class Actividadpostexamen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca el precio del plato: ");
+        double precio = Double.parseDouble(scanner.nextLine());
+        double total = 0;
+        while (precio > 0) {
+            total = precio + total;
+            System.out.println("Introduzca el precio del siguiente plato: ");
+            precio = Double.parseDouble(scanner.nextLine());
+        }
+        System.out.println("El precio total de la comida es de: " + total + " euros.");
+        scanner.close();
+    }
+}
