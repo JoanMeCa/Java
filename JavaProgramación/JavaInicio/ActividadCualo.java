911c9fc5d0e3bcd5c0a6308672726adaa233aa8a
Joan Menendez Calafell <jmenendezc@alumnes.santjosepobrer.es>
2023-11-02 18:41:33 +0100

    cualo

---
+++ JavaProgramación/JavaInicio/ActividadCualo.java
@@ -0,0 +1,16 @@
+package JavaInicio;
+
+public class ActividadCualo {
+    public static void main(String[] args) {
+        String frase = "Skibidi pop pop what you say, skibidi pop pop hee hee";
+        int contador = 0;
+        char[] array = frase.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == ' ') { 
+                contador = contador + 1;
+            }
+        }
+        System.out.println(frase); 
+        System.out.println("La frase tiene un total de " + contador + " espacios en blanco");
+    }
+}
